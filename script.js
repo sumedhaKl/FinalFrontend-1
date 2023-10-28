94e511b280e604767df0c4233041bdea7e7f272a
Ishraq Hossain <ishraqhossain1738@gmail.com>
2023-10-28 14:32:21 -0700

    Links changed

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(function () {
     //WOW plugin init
     new WOW().init();
-
     //parallax effect for banner
     (function () {
         var posY;
@@ -12,16 +11,13 @@ $(function () {
         }
         window.addEventListener("scroll", paralax);
     })();
-
     function generateBooks(books) {
         // selects this html element:
         // <section class="works scroll" id="works">
         //      <div class="container">
         const container = document.querySelector("#works .container");
-
         // declare an empty string to add to the html since we add to it in the loops below
         let html = "";
-
         // get the number of rows we need
         // divide # of books by 3 and round up
         let rows = Math.ceil(books.length / 3);
@@ -32,7 +28,7 @@ $(function () {
             // each column has 3 items
             // j=0 to j=2 (0 1 2 is 3 times)
             for (let j = 0; j < 3; j++) {
-                // each row has 3 items + offset of the item in the current row and the rows are going left and right and then the columns are going up and down. It muliplies the row number by three and then adds the column number 
+                // each row has 3 items + offset of the item in the current row and the rows are going left and right and then the columns are going up and down. It muliplies the row number by three and then adds the column number
                 let idx = i * 3 + j;
                 // reached end of books array
                 if (idx >= books.length) break;
@@ -50,85 +46,73 @@ $(function () {
             }
             html += `</div>`;
         }
-
         container.innerHTML = html;
     }
-
     // Usage
     const books = [
         {
             imageLink: "https://www.takewalks.com/blog/wp-content/uploads/2013/05/gatsby.jpg",
             bookName: "The Great Gatsby!",
-            link: "https://example.com/great-gatsby",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://m.media-amazon.com/images/I/51JyKkTnuZL._SL500_.jpg",
             bookName: "Dreams From My Father",
-            link: "https://example.com/dreams-from-my-father",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://m.media-amazon.com/images/I/814oeXzxhML._AC_UF350,350_QL50_.jpg",
             bookName: "The Grapes of Wrath!",
-            link: "https://example.com/grapes-of-wrath",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://sdi4.chrislands.com/sdi/978/00/60/8/9780060888695.jpg",
             bookName: "To Kill A Mockingbird!",
-            link: "https://example.com/to-kill-a-mockingbird",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://i.etsystatic.com/37564271/r/il/a493cc/4399495403/il_fullxfull.4399495403_fa5v.jpg",
             bookName: "Farenheit 451!",
-            link: "https://example.com/fahrenheit-451",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://sterling-us.imgix.net/covers/9781435172319.jpg?auto=format&h=648",
             bookName: "Heart of Darkness!",
-            link: "https://example.com/heart-of-darkness",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://mpd-biblio-covers.imgix.net/9781509826643.jpg",
             bookName: "Moby-Dick!",
-            link: "https://example.com/moby-dick",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://images.booksense.com/images/675/121/9780385121675.jpg",
             bookName: "The Shining!",
-            link: "https://example.com/the-shining",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
             imageLink: "https://sdi4.chrislands.com/sdi/978/05/25/9/9780525948926.jpg",
-            bookName: "Atlas Shrugged!",
-            link: "https://example.com/atlas-shrugged",
+            bookName: "Atlas Shurgged!",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
-            imageLink: "https://m.media-amazon.com/images/I/41VWMa8-aeS._SY445_SX342_.jpg",
+            imageLink: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdubbing.fandom.com%2Fwiki%2FThe_Adventures_of_Tom_Sawyer&psig=AOvVaw1aHFHcqVETFRzIyBw33qWF&ust=1698429181873000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCPCD3fKjlIIDFQAAAAAdAAAAABAF",
             bookName: "The Adventures Of Tom Sawyer!",
-            link: "https://example.com/the-adventures-of-tom-sawyer",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
-            imageLink: "https://m.media-amazon.com/images/I/61O+dMkKUOL._SY445_SX342_.jpg",
-            bookName: "Little Women!",
-            link: "https://example.com/little-women ",
+            imageLink: "https://sdi4.chrislands.com/sdi/978/05/25/9/9780525948926.jpg",
+            bookName: "DEF!",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
         {
-            imageLink: "https://i.insider.com/52d6f0e16bb3f7823a99f741?width=800&format=jpeg&auto=webp",
-            bookName: "A Tree Grows in Brooklyn!",
-            link: "https://example.com/a-tree-grows-in-brooklyn",
-        },
-        {
-            imageLink: "https://m.media-amazon.com/images/I/91ZuwqPXv6L._SY466_.jpg",
-            bookName: "East of Eden!",
-            link: "https://example.com/east-of-eden",
-        },
-        {
-            imageLink: "https://m.media-amazon.com/images/I/91ZuwqPXv6L._SY466_.jpg",
-            bookName: "The Sun Also Rises!",
-            link: "https://example.com/the-sun-also-rises",
+            imageLink: "https://sdi4.chrislands.com/sdi/978/05/25/9/9780525948926.jpg",
+            bookName: "GHI!",
+            link: "http://127.0.0.1:5500/bookInfo.html",
         },
     ];
-
     window.addEventListener("DOMContentLoaded", (event) => {
         generateBooks(books);
     });
 });
+
